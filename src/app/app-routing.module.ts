9bd57b84ba3c46c4855d4b89f258dc06a0195f9c
Dileep Kumar Rana <dileep1998rana@gmail.com>
2019-08-18 19:05:05 +0530

    video section design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PodcastsComponent } from './podcasts/podcasts.component';
+import { VideoSummaryComponent } from './video-summary/video-summary.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'HomeComponent', pathMatch: 'full' },
+  { path: 'home',
+     component: HomeComponent,
+     children:[
+      {path:'', redirectTo:'index', pathMatch:'full'},
+      { path: 'podcast', component:PodcastsComponent  },
+      { path: 'index', component: VideoSummaryComponent},
+      {path:'**', redirectTo:'index', pathMatch:'full'}
+
+     ]
+  },
+  {path:'**', redirectTo:'home', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
